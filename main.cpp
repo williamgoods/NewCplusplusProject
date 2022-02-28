362ae6781efe007032ddab68fbbef0efd010f528
blackmanjaro <lw2957409438@gmail.com>
2022-02-28 12:19:42 +0800

    add for cpm test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <spdlog/spdlog.h>
 
 int main(int argc, char * argv[])
 {
+    spdlog::info("Hello world!");
     std::cout << "Hello, Cplusplus World!" << std::endl;
     return 0;
 }
--- script/run.sh
+++ script/run.sh
@@ -7,9 +7,11 @@ CMakePorjectNameStatic="CMAKE_PROJECT_NAME:STATIC="
 mkdir -p build
 
 cd build
-cmake -G Ninja ..
-ninja clean
-ninja
+#cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE=/opt/vcpkg/scripts/buildsystems/vcpkg.cmake ..
+cmake -P ../script/CMakeLists.txt
+cmake -G Ninja .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+#ninja clean
+ninja -j 32
 
 cat CMakeCache.txt | while read line
 do
